ba69f881ddf1c5d1bf2471c955bcedb18fb6c97c
mahmud aremu <aremumahmud2003@gmail.com>
2024-02-03 00:36:05 +0100

    whats the worst that can happen

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,43 @@
-import Header from "./components/header"
-import Hero from "./components/hero"
-import './App.css'
-import About from "./components/About"
-import Images from "./components/images"
-import Services from "./components/Services"
-import ServiceImages from "./components/service_images"
-import Clients from "./components/clients"
-import ClientImages from "./components/client_image"
-import Footer from "./components/Footer"
-import 'animate.css'
-import Spiral from "./components/spiral"
-
-  
+import Header from "./components/header";
+import Hero from "./components/hero";
+import "./App.css";
+import About from "./components/About";
+import Images from "./components/images";
+import Services from "./components/Services";
+import ServiceImages from "./components/service_images";
+import Clients from "./components/clients";
+import ClientImages from "./components/client_image";
+import Footer from "./components/Footer";
+import "animate.css";
+import Spiral from "./components/spiral";
 
 function App() {
-    return (
-      <div>
-        <Spiral />
-          <Header />
-         <Hero />
-        
+  return (
+    <div>
+      <Spiral />
+      <Header />
+      <Hero />
+      <br />
+      <About />
+      <Images />
+      <Services>
+        {" "}
+        <ServiceImages /> <br />
         <br />
-        <About />
-         <Images /> 
-         <Services >  <ServiceImages /> <br /><br /></Services>
-         <Clients >  
-          <ClientImages /> <br /><br /><br /><br /> <br /><br /><br /><br />
-          <Footer />
-          </Clients> {/* */}
-         
-      </div>
-     
-
-    )
+      </Services>
+      <Clients>
+        <ClientImages /> <br />
+        <br />
+        <br />
+        <br /> <br />
+        <br />
+        <br />
+        <br />
+        <Footer />
+      </Clients>{" "}
+      {/* */}
+    </div>
+  );
 }
 
-export default App
+export default App;
